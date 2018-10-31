97841b3ab7d7929ee5dd51a744e58de338f5b8dc
zhanghuan <zezhang@actiontec.com>
2018-10-31 13:09:03 +0800

    网页响应式调整

--- js/help.js
+++ js/help.js
@@ -1,7 +1,7 @@
 $(function(){
     /*跳转到首页 */
     $("#Home_page").click(function(){
-        location.href = "Shar-ai.html";
+        location.href = "sharpai-index.html";
     })
     
     /**
--- js/related.js
+++ js/related.js
@@ -8,7 +8,7 @@ $(function() {
     })
     /*跳转到首页 */
     $("#Home_page").click(function(){
-        location.href = "Shar-ai.html";
+        location.href = "sharpai-index.html";
     })
    
 })

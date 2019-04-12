52ef4c0ca00e2a5273fbbbee36cbff84e078d97b
zhanghuanll <40556850+zhanghuanll@users.noreply.github.com>
2019-04-12 14:04:05 +0800

    整理新的sharpai页面 (#18)
    * 添加新的sharpai页面
    
    * 整理新的sharpai页面

---
+++ sharpai_io_before/js/sharp-index.js
@@ -0,0 +1,27 @@
+$(function(){
+    
+    /**
+     * 跳转到帮助中心
+     */
+    $("#Help_center").click(function(){
+        location.href = "help.html";
+    })
+
+    //跳转到联系我们
+    $("#Help_center").click(function(){
+        location.href = "Help_center.html";
+    })
+    /**
+     *　跳转到淘宝购买商品
+     */
+    $("#location-btn button").click(function(){
+        location.href ="https://item.taobao.com/item.htm?id=565957238941https://item.taobao.com/item.htm?spm=a1z10.1-c.w4004-12617595122.3.33ff4679kgbl66&id=577748526182";
+    })
+
+    /**
+     * 跳转到了解更多
+     */
+    $(".buy-fa").click(function(){
+        location.href="related.html";
+    });
+})
